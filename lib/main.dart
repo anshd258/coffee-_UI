80b6e5675a1c0e51cab82fa46e407ed3393d330a
atishayjain <atishay.jain@cred.club>
2023-02-19 00:50:20 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -30,24 +30,31 @@ import './page/notificationpg.dart';
 import './page/Admin/homepage.dart';
 import './page/Admin/allOrders.dart';
 import './page/Admin/account.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const mainApp());
+// ...
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
 }
 
 bool isAdmin = false;
 
-class mainApp extends StatefulWidget {
-  const mainApp({super.key});
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
 
   @override
-  State<mainApp> createState() => _mainAppState();
+  State<MainApp> createState() => _MainAppState();
 }
 
-class _mainAppState extends State<mainApp> {
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
-    
     return MultiProvider(
         providers: [
           ChangeNotifierProvider<AllOrderProvider>(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:inter_coffee/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const mainApp());
+    await tester.pumpWidget(const MainApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
